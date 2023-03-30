0fbc059bdeb1f293b6e4143203c07ce9047a3310
SADIKUR RAHMAN SAGOR <sadikursagor414@gmail.com>
2023-03-30 16:48:23 +0600

    added cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Cart.css'
+const Cart = ({ cart }) => {
+    //const cart = props.cart; option 1
+    // const {cart} = props ;  //option 2
+    let totalPrice = 0;
+    let totalShiping = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price;
+        totalShiping = totalShiping + product.shipping;
+    }
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShiping + tax;
+
+    return (
+        <div className='cart'>
+            <h4>Order Summary </h4>
+            <p>Select Items: {cart.length}</p>
+            <p>Total price : ${totalPrice} </p>
+            <p>Total Shiping :${totalShiping} </p>
+            <p>Tax : {tax.toFixed(2)} </p>
+            <h6>Grand Total:${grandTotal.toFixed(2)} </h6>
+        </div>
+    );
+};
+
+export default Cart;
